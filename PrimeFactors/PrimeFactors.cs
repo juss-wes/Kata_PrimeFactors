f314ed35e13f8dcfb7ac4b010c6f1af5bbf751ee
Wesley Vollmar <wcvollmar@marathonpetroleum.com>
2018-03-20 13:13:23 +0000

    Updated PrimeFactors.cs for consistency with the completed solution

--- PrimeFactors/PrimeFactors.cs
+++ PrimeFactors/PrimeFactors.cs
@@ -14,9 +14,9 @@ namespace PrimeFactors
         /// <summary>
         /// Method to generate the prime factors for a given input
         /// </summary>
-        /// <param name="input">Integer to determine </param>
-        /// <returns></returns>
-        public static List<int> Generate(int input)
+        /// <param name="input">Integer to determine the prime factors for</param>
+        /// <returns>List of integers found to be primes</returns>
+        public static IEnumerable<int> Generate(int input)
         {
             // TODO - Implement here
             throw new NotImplementedException();
